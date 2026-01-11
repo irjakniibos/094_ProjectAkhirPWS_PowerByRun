91081bf294ae41a460df88241d4967be8cf3dd6c
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-11 19:16:01 +0700

    Menambahkan konfigurasi database menggunakan Sequelize dan variabel lingkungan

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    timezone: "+07:00",
+  }
+);
+
+module.exports = sequelize;
